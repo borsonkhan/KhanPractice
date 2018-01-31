d6229f954f88445ca262dc45837325be2ab89149
Borson Khan <borsonkh@gmail.com>
2018-01-31 01:19:36 -0800

    first method

--- src/khan/practice/KhanPractice.java
+++ src/khan/practice/KhanPractice.java
@@ -1,5 +1,24 @@
 package khan.practice;
 
 public class KhanPractice {
+	
+	public static void main(String[] args) {
+		System.out.println("Q1 = "+ getText("khan"));
+		
+	}
+	
+    /*
+     * Q1 = write a method that take one String input and return same as uppercase;
+     * input = String name;
+     * return type = String;
+     * serving bucket = variable;
+     */
+	public static String getText (String name) {
+		String text = " ";
+		text = name.toUpperCase();
+		return text;
+	}
+	
+	
 
 }
