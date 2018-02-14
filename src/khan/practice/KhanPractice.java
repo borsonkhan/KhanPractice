0b3803b791ad28b9b5ecfb789b406dadb03508b8
Borson Khan <borsonkh@gmail.com>
2018-02-14 04:06:42 -0800

    method 6

--- src/khan/practice/KhanPractice.java
+++ src/khan/practice/KhanPractice.java
@@ -10,26 +10,34 @@ public class KhanPractice {
 		System.out.println("Q3 = "+ getLowerText("BORSON"));
 		System.out.println("Q4 = "+ getEvenText(20));
 		System.out.println("Q5 = "+ getOddText(20));
+		System.out.println("Q6 = "+ getKilo(3));
+		
 		
 	}
-	/*
-	 * Q4 = write a method that take one number input and return the list of all even number from o to that given number
-	 * input = int number; 
+	/**
+	 * Q7 = write a method that take one string input and return a list of all index; 
+	 * input = String input ; 
 	 * return type = ArrayList<Integer>; 
 	 * serving bucket = list;
 	 */
-	public static ArrayList<Integer> getEvenText(int number){
-		ArrayList<Integer> text = new ArrayList<Integer>();
+
+	/*
+	 * Q6 = write a method that take int input as mile and return as a kilometer; 
+	 * input = int number; 
+	 * return = int;
+	 * serving = variable;
+	 */
+	public static int getKilo(int mile) {
+		int kilo = 0;
 		
-		for(int i=0; i<=number;i++) {
-			
-			if(i%2==0) {
-				
-				text.add(i);
-			}
-		}
-		return text;
+		kilo = mile * 106;
+		
+		
+		return kilo;
 	}
+	
+	
+	
 	/*
 	 * Q5= write a method a number input and return the list of all odd number from 0 to that given number;
 	 * input = int number; 
@@ -47,6 +55,25 @@ public class KhanPractice {
 			}
 		}
 		
+		return text;
+	}
+	
+	/*
+	 * Q4 = write a method that take one number input and return the list of all even number from o to that given number
+	 * input = int number; 
+	 * return type = ArrayList<Integer>; 
+	 * serving bucket = list;
+	 */
+	public static ArrayList<Integer> getEvenText(int number){
+		ArrayList<Integer> text = new ArrayList<Integer>();
+		
+		for(int i=0; i<=number;i++) {
+			
+			if(i%2==0) {
+				
+				text.add(i);
+			}
+		}
 		return text;
 	}
 	
